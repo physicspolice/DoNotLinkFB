adf6087ab0be181d8010af816d6b30d51f0d31e1
Physics Police <physics.police@gmail.com>
2014-07-27 03:06:06 -0700

    Image download.

--- index.php
+++ index.php
@@ -94,6 +94,8 @@ if($_POST['action'] == 'create')
 		$errors[] = 'Title is too long.';
 	if(strlen($link['description']) > 255)
 		$errors[] = 'Description is too long.';
+	if(!($image = @file_get_contents($link['image'])))
+		$errors[] = 'Could not download image.';
 	if($errors)
 		die(json_encode(array('errors' => $errors)));
 	$query = $db->prepare('
@@ -106,6 +108,26 @@ if($_POST['action'] == 'create')
 		$success = $query->execute($link);
 	}
 	while(!$success);
+	file_put_contents("images/{$link['key']}", $image);
+	$type = exif_imagetype("images/{$link['key']}");
+	$allowed = array(
+		IMAGETYPE_GIF  => 'gif',
+		IMAGETYPE_JPEG => 'jpg',
+		IMAGETYPE_PNG  => 'png',
+		IMAGETYPE_BMP  => 'bmp',
+	);
+	if(isset($allowed[$type]))
+	{
+		$query = $db->prepare('UPDATE `link` SET `type` = ? WHERE `key` = ?');
+		$query->execute(array($allowed[$type], $link['key']));
+		rename("images/{$link['key']}", "images/{$link['key']}.{$allowed[$type]}");
+	}
+	else
+	{
+		$query = $db->prepare('UPDATE `link` SET `image` = NULL WHERE `key` = ?');
+		$query->execute(array($link['key']));
+		unlink("images/{$link['key']}");
+	}
 	die(json_encode(array(
 		'link' => "http://{$_SERVER['HTTP_HOST']}/{$link['key']}"
 	)));
--- install.php
+++ install.php
@@ -10,8 +10,12 @@ $result = $db->exec('CREATE TABLE IF NOT EXISTS `link` (
 	`url` text NOT NULL,
 	`title` text NOT NULL,
 	`description` text NOT NULL,
-	`image` text NOT NULL,
+	`image` text NULL,
+	`type` char(3) NOT NULL,
 	`ip` varchar(15) NOT NULL,
 	`time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`key`)
 )');
+
+if(!file_exists('images'))
+	mkdir('images');
